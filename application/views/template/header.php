13188546c7dd4cd4bbb9130eb694098d3c6e8953
nandaborneo <nandarm.sti@gmail.com>
2018-11-02 09:41:51 +0700

    First commit

---
+++ application/views/template/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CiBlog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css"/>
+    </head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="<?= base_url();?>">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url();?>">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url();?>about">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <div class="container">
+            
+    
